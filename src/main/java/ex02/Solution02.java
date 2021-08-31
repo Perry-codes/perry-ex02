66af3b1e6912e4434bf5b1cc888f1b90fa532d07
McLovin4 <89555259+McLovin4@users.noreply.github.com>
2021-08-30 23:04:35 -0400

    Solution 2 initial commit

---
+++ src/main/java/ex02/Solution02.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Maggie Perry
+ */
+
+/*
+establish scanner input from user
+
+prompt user to input string
+
+count characters in string
+
+print back original string with character count
+
+ */
+
+package ex02;
+
+import java.util.Scanner;
+
+public class Solution02 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is the input string? ");
+
+        String inputString = sc.nextLine();
+
+        int stringLength = inputString.length();
+
+        System.out.printf("%s has %d characters." , inputString , stringLength);
+
+
+    }
+}
